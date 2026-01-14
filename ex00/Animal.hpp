3455199150d583b5af08ce3bb16945959138e163
zajaddou <Zakariaajaddou@gmail.com>
2026-01-14 10:40:44 +0100

    update

--- ex00/Animal.hpp
+++ ex00/Animal.hpp
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Animal.hpp                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: zajaddou <zajaddou@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/13 10:14:45 by zajaddou          #+#    #+#             */
-/*   Updated: 2026/01/13 10:59:52 by zajaddou         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #ifndef ANIMAL_HPP
 # define ANIMAL_HPP
@@ -16,15 +5,7 @@
 # include <iostream>
 
 class Animal {
-    protected:
-        std::string type;
-    public:
-        Animal();
-        Animal(const Animal &src);
-        Animal &operator=(const Animal &src);
-        virtual ~Animal();
-        virtual void makeSound() const;
-        std::string getType() const;
+
 };
 
 #endif
