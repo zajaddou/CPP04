a4c753030f17e31807022ef44066cfc39f169f56
zajaddou <Zakariaajaddou@gmail.com>
2026-01-20 12:03:11 +0100

    update

--- ex01/Brain.cpp
+++ ex01/Brain.cpp
@@ -16,11 +16,8 @@ Brain& Brain::operator=(const Brain& src)
 {
     std::cout << "Brain - copy assignment" << std::endl;
     if (this != &src)
-    {
-        int i = -1;
-        while ( ++i < 100 )
+        for (int i = 0; i < 100; i++)
             this->ideas[i] = src.ideas[i];
-    }
     return (*this);
 }
 
